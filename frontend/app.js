d64e550942a810473df44b2ced481e46f3d91342
martinanajdovska <martina_najdovska@hotmail.com>
2025-09-15 20:57:02 +0200

    fix

--- frontend/app.js
+++ frontend/app.js
@@ -1,39 +1,37 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const messageList = document.getElementById('messages');
-    const form = document.getElementById('message-form');
+const messageList = document.getElementById('messages');
+const form = document.getElementById('message-form');
 
-    if (messageList) {
-        fetchMessages();
-    }
+if (messageList) {
+    fetchMessages();
+}
 
-    if (form){
-        form.addEventListener('submit', async (e) => {
-            e.preventDefault();
+if (form) {
+    form.addEventListener('submit', async (e) => {
+        e.preventDefault();
 
-            const text = document.getElementById('text').value.trim();
-            const receiver = document.getElementById('receiver').value.trim();
+        const text = document.getElementById('text').value.trim();
+        const receiver = document.getElementById('receiver').value.trim();
 
-            try {
-                const response = await fetch('http://localhost:8080/api/add', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/x-www-form-urlencoded',
-                    },
-                    body: new URLSearchParams({text, receiver})
-                });
+        try {
+            const response = await fetch('http://localhost:8080/api/add', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                },
+                body: new URLSearchParams({text, receiver})
+            });
 
-                if (response.ok) {
-                    window.location.href = 'index.html';
-                } else {
-                    alert('Failed to submit the message.');
-                }
-            } catch (err) {
-                console.error('Error submitting message:', err);
-                alert('Error submitting message.');
+            if (response.ok) {
+                window.location.href = 'index.html';
+            } else {
+                alert('Failed to submit the message.');
             }
-        });
-    }
-});
+        } catch (err) {
+            console.error('Error submitting message:', err);
+            alert('Error submitting message.');
+        }
+    });
+}
 
 async function fetchMessages() {
     try {
